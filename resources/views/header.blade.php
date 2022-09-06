b3c32743034da933aaaebc309e9d47d6703ee64f
sumankr0511 <sumanankan4@gmail.com>
2022-09-06 17:56:08 +0530

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">E.Comm</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Disabled</a>
+      </li>
+      <li>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+</li><br>
+<li>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <ul>
+      
+        <a class="nav-link" href="#">Add to Cart</a>
+      
+      </ul>
+    </form>
+  </div>
+</nav>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>E-comm</title>
+  </head>
+  <body>
+    {{ View::make('header')}}
+     @yield('content')
+     {{ View::make('footer')}}
+
+
+
+  
+  </body>
+  <style>
+    .log{
+        height: 500px;
+        padding-top: 100px;
+        align-items: center;
+        margin-left: 40%;
+    }
+    
+  </style>
+  </html>
